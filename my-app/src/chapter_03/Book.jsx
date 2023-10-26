84e8a5f40c6d2124ab3646acf31d826fe82cd205
ssoong12 <ssoong12@naver.com>
2023-10-26 14:00:27 +0900

    JSX

--- my-app/src/chapter_03/Book.jsx
+++ my-app/src/chapter_03/Book.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// JSX 문법을 사용했을 때
 function Book(props) {
   return (
     <div>
@@ -8,5 +9,25 @@ function Book(props) {
     </div>
   );
 }
+
+// JSX 문법을 사용하지 않았을 때
+// function Book(props) {
+//   return React.createElement(
+//     'div',
+//     null,
+//     [
+//       React.createElement(
+//         'h1',
+//         null,
+//         `이 책의 이름은 ${props.name}입니다.`
+//       ),
+//       React.createElement(
+//         'h2',
+//         null,
+//         `이 책은 총 ${props.numOfPage}페이지로 이뤄져 있습니다.`
+//       )
+//     ]
+//   )
+// }
 
 export default Book;
